e75157f3c2dfdcd6e9117db9c0ea4a9ef7698e84
Prawy_126 <michal190713@gmail.com>
2023-07-08 11:19:11 +0200

    pętle w C#

---
+++ lekcja5/lekcja5/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace lekcja5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pętla for");
+            for (int a = 0; a < 100; a++)
+            {
+                Console.WriteLine(a);
+            }
+            int i = 0;
+            Console.WriteLine("Pętla while");
+            while (i < 100)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            i = 0;
+            Console.WriteLine("Pętla do while");
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i < 100);
+            Console.WriteLine("Pętla for przerwanie po 5");
+            for(int a = 0; a < 100; a++)
+            {
+                Console.WriteLine(a);
+                if(a== 5)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Wypisywanie liczb nie parzystych");
+            for(int a = 0; a < 100; a++)
+            {
+                Console.WriteLine(a);
+                if (a % 2 == 0)
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}

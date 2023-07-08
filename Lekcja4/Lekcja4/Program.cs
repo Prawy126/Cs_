b5414756a839d9c29ed097ab974ff6824c48fcae
Prawy_126 <michal190713@gmail.com>
2023-07-08 11:05:59 +0200

    instrukcje warunkowe w C#

---
+++ Lekcja4/Lekcja4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lekcja4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            bool b = a > 0;
+
+            if (b)
+            {
+                Console.WriteLine(a + " jes większe od 0");
+            }
+            else
+            {
+                Console.WriteLine(a + " nie jest większe od 0");
+            }
+
+            if (b) Console.WriteLine("Warunek jest prawdziwy"); Console.WriteLine("Warunek jest prawdziwy");
+
+        }
+    }
+}

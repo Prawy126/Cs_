48eb591831d0ac4d56507c5e00964d309b309c48
Prawy_126 <michal190713@gmail.com>
2023-07-10 11:57:17 +0200

    tablice w C#

---
+++ Lekcja10/Lekcja10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lekcja10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica = new int[365];
+            string[] dni = { "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela" };
+            for(int i = 0; i < dni.Length; i++)
+            {
+                Console.WriteLine(dni[i]);
+            }
+           
+           
+
+        }
+    }
+}

9038f93e64baf120fff906086e3d467f4d0933b1
Prawy_126 <michal190713@gmail.com>
2023-07-14 11:17:44 +0200

    funkcje w C# które coś zwracają

---
+++ Lekcja13/Lekcja13/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lekcja13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Dodaj(2,4);
+            Console.WriteLine("Wynik dodawania wynosi " + a);
+        }
+
+        static int Dodaj(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}

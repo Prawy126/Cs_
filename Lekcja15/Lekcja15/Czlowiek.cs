a3bf22a743bb677bfe771b78566e53988d273666
Prawy_126 <michal190713@gmail.com>
2023-07-15 12:35:04 +0200

    Wstęp do obiektowości w C#

---
+++ Lekcja15/Lekcja15/Czlowiek.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja15
+{
+    internal class Czlowiek
+    {
+        public string imie = "Adrain", nazwisko = "Kowalski";
+        public void PrzedstawSie()
+        {
+            //this działa tak samo ja w java 
+            Console.WriteLine("Mam na imię: " + this.imie + " na nazwisko: " + this.nazwisko);
+        }
+    }
+}
---
+++ Lekcja15/Lekcja15/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lekcja15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Czlowiek czlowiek = new Czlowiek();
+            Console.WriteLine(czlowiek.imie);
+            Czlowiek czlowiek2 = new Czlowiek();
+            czlowiek2.imie = "Bartek";
+            Console.WriteLine(czlowiek2.imie);
+            czlowiek.PrzedstawSie();
+            czlowiek2.nazwisko = "Nowak";
+            czlowiek2.PrzedstawSie();
+        }
+    }
+
+    
+}

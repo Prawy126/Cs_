74f227b8becbf7d46a3e9b3dd70c01129859e20f
Prawy_126 <michal190713@gmail.com>
2023-07-16 14:49:49 +0200

    Konstruktory w C#

---
+++ Lekcja16/Lekcja16/Czlowiek.cs
@@ -0,0 +1,26 @@
+﻿namespace Lekcja16
+{
+    internal class Czlowiek
+    {
+        public Czlowiek(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+        public Czlowiek()
+        {
+            Console.WriteLine("Imię i nazwisko zostało ustawione na domyślne");
+        }
+        public string imie = "Adrian", nazwisko = "Kowalski";
+
+        public void PrzedstawSie()
+        {
+            Console.WriteLine("Mam na imię " + this.imie + "\nMam na nazwisko " + this.nazwisko);
+        }
+        ~Czlowiek()
+        {
+            System.Diagnostics.Trace.WriteLine("Jestem destruktorem");
+        }
+
+    }
+}
---
+++ Lekcja16/Lekcja16/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lekcja16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Czlowiek czlowiek = new Czlowiek();
+            czlowiek.PrzedstawSie();
+            Czlowiek czlowiek2 = new Czlowiek("Michał", "Nowak");
+            //czlowiek2.PrzedstawSie();
+            czlowiek.PrzedstawSie();
+            
+
+        }
+    }
+}

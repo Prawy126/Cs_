22a8cc7251ed55f54531b0cc1ca790b842d26a33
Prawy_126 <michal190713@gmail.com>
2023-07-22 17:18:39 +0200

    Gettery i Settery w C#

---
+++ Lekcja18/Lekcja18/Czlowiek.cs
@@ -0,0 +1,40 @@
+﻿namespace Lekcja18
+{
+    class Czlowiek
+    {
+        public string imie;
+        private int wiek;
+        /* public void SetWiek(int wiek)
+         {
+             if (wiek >=0) {
+                 this.wiek = wiek;
+             }else
+             {
+                 wiek = Math.Abs(wiek);
+                 this.wiek = wiek;
+             }
+         }
+         public int GetWiek()
+         {
+             return wiek;
+         }*/
+        //public int wiek { get; set; }
+
+        public int Wiek
+        {
+            get { return wiek; }
+            set
+            {
+
+                if (value >= 0 && value <= 200)
+                {
+                    wiek = value;
+                }
+                else
+                {
+                    wiek = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Lekcja18/Lekcja18/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lekcja18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Czlowiek czlowiek = new Czlowiek();
+            czlowiek.imie = "Adraian";
+            czlowiek.Wiek = 20;
+            Console.WriteLine($"Jestem {czlowiek.imie} lat {czlowiek.Wiek}");
+        }
+    }
+}

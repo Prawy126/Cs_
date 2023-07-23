e7f98ac368963b0f3707c49c0d63f19bba808856
Prawy_126 <michal190713@gmail.com>
2023-07-23 16:04:14 +0200

    Operacje na stringach w C#

---
+++ Lekcja19/Lekcja19/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lekcja19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = GenerujTekst("Mam na imię #0 i mam #1 lat", "Adam", 22);
+            Console.WriteLine(tekst);
+        }
+        //params przyjmuje nieograniczoną liczbę parametrów i musi być zawsze ostatni
+        public static string GenerujTekst(string tekst, params object[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                tekst = tekst.Replace("#" + i, args[i].ToString());
+            }
+            return tekst;
+        } 
+    }
+}

de289e58aef1a9e8499e8612c1f2d2301fc049aa
Prawy_126 <michal190713@gmail.com>
2023-07-25 16:29:41 +0200

    zmiana domyślnych znaków w C#

---
+++ lekcja21/lekcja21/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+namespace lekcja21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            //foreach(var item in c)
+            //{
+            //    Console.Write(item.Name);
+            //    Console.WriteLine(item.NumberFormat.NumberDecimalSeparator);
+            //}
+            Console.WriteLine(CultureInfo.CurrentCulture.Name);
+           // CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator = ";";
+            var newCulture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            newCulture.NumberFormat.NumberDecimalSeparator = ";";
+            CultureInfo.CurrentCulture = newCulture;
+            int a = -5;
+            float b = 3.14f;
+            Console.WriteLine($"{a}\n {b}");
+        }
+    }
+}

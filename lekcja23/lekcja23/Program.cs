63314bcf96fcc1987b5fa807f62b342117c6cc2e
Prawy_126 <michal190713@gmail.com>
2023-07-31 14:16:22 +0200

    Kolekcja List w C#

---
+++ lekcja23/lekcja23/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace lekcja23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Auto> list = new List<Auto>();
+            Auto bmw = new Auto("BMW");
+            Auto ford = new Auto("Ford");
+            Auto audi = new Auto("Audi");
+            list.Add(bmw);
+            list.Add(audi);
+            list.Add(ford);
+
+            foreach (Auto item in list)
+            {
+                Console.WriteLine(item.Nazwa);
+            }
+        }
+    }
+
+    class Auto
+    {
+        public string Nazwa { get; set; }
+        public Auto(string nazwa)
+        {
+            this.Nazwa = nazwa;
+        }
+        public void UruchomSilnik()
+        {
+            Console.WriteLine("Uruchomiono silnik " + Nazwa);
+        }
+    }
+}

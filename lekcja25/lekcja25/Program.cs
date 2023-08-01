3e584050b9d65f500c0f31c3a8ea12f0d9eb42ea
Prawy_126 <michal190713@gmail.com>
2023-08-01 11:52:11 +0200

    Stos i Kolejka w C#

---
+++ lekcja25/lekcja25/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace lekcja25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            Console.WriteLine("Kolejka:");
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Peek());
+            
+            Console.WriteLine("Stos:");
+            Stack<int> stos = new Stack<int>();
+            stos.Push(1);
+            stos.Push(2);
+            stos.Push(3);
+            Console.WriteLine(stos.Peek());
+            Console.WriteLine(stos.Pop());
+            Console.WriteLine(stos.Peek());
+        }
+    }
+}

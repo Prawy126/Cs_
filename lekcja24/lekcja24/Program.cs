f97b5fdcbcf3ba7239d625cbc432ae21124e8c29
Prawy_126 <michal190713@gmail.com>
2023-08-01 11:24:01 +0200

    Kolekcja Dicitory w C#

---
+++ lekcja24/lekcja24/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace lekcja24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictonary = new Dictionary<int, string>();
+            dictonary.Add(1, "Ania");
+            dictonary.Add(2, "Bartek");
+            Console.WriteLine(dictonary.Count);
+            foreach (var item in dictonary)
+            {
+                Console.WriteLine("{0} : {1}", item.Key, item.Value);
+            }
+        }
+    }
+}

17ee97a93e70d30ee2d928979e5df3c5ddbc1fa0
Prawy_126 <michal190713@gmail.com>
2023-08-02 11:32:06 +0200

    porówanie w C#

---
+++ lekcja26/lekcja26/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace lekcja24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto1 = new Auto("FIAT", 1999);
+            Auto auto2 = new Auto("FIAT", 1999);
+            Console.WriteLine(auto1.Equals(auto2));
+        }
+    }
+
+    class Auto
+    {
+        public string Model { get; set; }
+        public int Rocznik { get; set; }
+        public Auto(string Model, int Rocznik)
+        {
+            this.Rocznik = Rocznik;
+            this.Model = Model;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+        
+        public bool Equals(Auto auto)
+        {
+            return auto.Model == this.Model && auto.Rocznik == this.Rocznik;
+        }
+    }
+}

b93c777c3ec75bc7e06af2c2f5ee7cf017775358
Prawy_126 <michal190713@gmail.com>
2023-08-03 14:19:11 +0200

    kolejne zagadnienie związane z dziedziczeniem w C#

---
+++ lekcja29/lekcja29/Mag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lekcja29
+{
+    class Mag : Postac
+    {
+        public Mag(string imie, int punktyHP, int mana) : base (imie, punktyHP)
+        {
+            this.imie = imie;
+            this.punktyHP = punktyHP;
+            this.mana = mana;
+        }
+
+        public int mana;
+    }
+}
---
+++ lekcja29/lekcja29/Postac.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lekcja29
+{
+    class Postac
+    {
+        public Postac(string imie, int punktyHP)
+        {
+            this.imie = imie;
+            this.punktyHP = punktyHP;
+        }
+        public string imie;
+        public int punktyHP;
+    }
+}

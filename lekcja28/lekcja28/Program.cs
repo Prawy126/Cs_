56824bdbc25f68feae02adc60a59e84e40d82e6c
Prawy_126 <michal190713@gmail.com>
2023-08-03 14:03:35 +0200

    dziedziczenie w C#

---
+++ lekcja28/lekcja28/Program.cs
@@ -0,0 +1,17 @@
+﻿using lekcja28;
+
+namespace Lekcja28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ziemia ziemia = new Ziemia();
+            ziemia.Nazwa = "Mars";
+            ziemia.RuchObrotowy();
+            ziemia.Atomsfera();
+            ziemia.PoraDniaINocy();
+        }
+
+    }
+}
---
+++ lekcja28/lekcja28/Ziemia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lekcja28
+{
+    class Ziemia : Planeta
+    {
+       
+        public void Atomsfera()
+        {
+            Console.WriteLine("atmosfera Ziemska");
+        }
+    }
+}

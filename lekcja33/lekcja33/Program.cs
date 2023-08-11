d3a41e0397dd712b6a5dde7d136ce37edf0bcb1b
Prawy_126 <michal190713@gmail.com>
2023-08-11 13:21:26 +0200

    Typ Enum w C#

---
+++ lekcja33/lekcja33/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace lekcja33
+{
+    class Program
+    {
+
+        enum PoryRoku : long
+        {
+            //każda zmienna jest numerowana domyślnie od 0
+            Wiosn , Lato, Jesień, Zima
+        }
+
+        static void Main(string[] args)
+        {   //można też rzutować liczby na typ enum
+            PoryRoku pory = (PoryRoku)1;
+            //można przeżutować enum na int
+            Console.WriteLine(pory);
+        }
+    }
+}

a084751caa98642c2807cb37e23532786ae5f8bb
Prawy_126 <michal190713@gmail.com>
2023-08-31 14:52:03 +0200

    Własne wyjątki w C#

---
+++ lekcja35/lekcja35/DivideByFiveException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lekcja35
+{
+    class DivideByFiveException : Exception
+    {
+        public DivideByFiveException() 
+        {
+            Console.WriteLine("Błąd");
+        }
+        public DivideByFiveException(string message) : base(message + " dzielenia przez 5!")
+        {
+
+        }
+
+        
+    }
+}
---
+++ lekcja35/lekcja35/Program.cs
@@ -0,0 +1,32 @@
+﻿using lekcja35;
+
+namespace lecka35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Wynik = " + Dzielenie(5));
+            }catch(DivideByFiveException e)
+            {
+                Console.WriteLine("Dzielisz przez pięć!!!" + e.Message);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Coś poszło nie tak");
+            }
+        }
+
+        public static float Dzielenie(int a)
+        {
+            if(a == 5)
+            {
+                throw new DivideByFiveException("Wystąpił błąd");
+            }
+            return 10 / a;
+        }
+    }
+}
